ec856add62d155b15d3f4097653a3e9044d4d070
Lucky Garg <lucky.work.005@gmail.com>
2026-03-23 15:03:55 +0530

    Time: 16 ms (48.72%) | Memory: 17 MB (50.03%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        for (auto it : edges) {
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+            dist[u][v] = w;
+            dist[v][u] = w;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    dist[i][j] = 0;
+                }
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX) {
+                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+        int maxW = n + 1;
+        int Mcity = -1;
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (dist[i][j] <= distanceThreshold) {
+                    cnt++;
+                }
+            }
+            if (cnt <= maxW) {
+                maxW = cnt;
+                Mcity = i;
+            }
+        }
+        return Mcity;
+    }
+};
