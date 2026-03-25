c4a58438fa5da8c046104c4843504880ee564811
Lucky Garg <lucky.work.005@gmail.com>
2026-03-25 11:42:37 +0530

    Time: 730 ms (7.66%) | Memory: 9.8 MB (0.85%) - LeetSync

--- 1401-number-of-burgers-with-no-waste-of-ingredients/number-of-burgers-with-no-waste-of-ingredients.cpp
+++ 1401-number-of-burgers-with-no-waste-of-ingredients/number-of-burgers-with-no-waste-of-ingredients.cpp
@@ -3,15 +3,18 @@ public:
     vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
         int n = cheeseSlices;
         if(tomatoSlices == 0 && cheeseSlices == 0)return {0,0};
-        if(tomatoSlices == 2 && cheeseSlices == 1)return {0,1};
-        if(cheeseSlices> tomatoSlices)return {};
         else if(tomatoSlices % 2 == 1)return {};
         else{
-            while(cheeseSlices > 0){
-                if((cheeseSlices*4 +(n - cheeseSlices)*2) == tomatoSlices){
-                    return {cheeseSlices , n-cheeseSlices};
+            // while(cheeseSlices > 0){
+            //     if((cheeseSlices*4 +(n - cheeseSlices)*2) == tomatoSlices){
+            //         return {cheeseSlices , n-cheeseSlices};
+            //     }
+            //     cheeseSlices--;
+            // }
+            for(int i = 0 ; i < n ; i++){
+                if((i*4 + (cheeseSlices - i)*2 == tomatoSlices) && i+cheeseSlices-i == n){
+                    return {i,cheeseSlices - i};
                 }
-                cheeseSlices--;
             }
         }
         return {};
