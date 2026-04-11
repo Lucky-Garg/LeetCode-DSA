48d4829a2c29b4faa61a55b91a8c89d28b8b5bf9
Lucky Garg <lucky.work.005@gmail.com>
2026-04-11 12:32:40 +0530

    Time: 1 ms (93.96%) | Memory: 123.9 MB (84.25%) - LeetSync

--- 2455-node-with-highest-edge-score/node-with-highest-edge-score.cpp
+++ 2455-node-with-highest-edge-score/node-with-highest-edge-score.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     int edgeScore(vector<int>& edges) {
-        vector<vector<int>>adj(edges.size());
-        for(int i = 0 ; i <edges.size() ; i++){
-            adj[edges[i]].push_back(i);
+        // vector<vector<int>>adj(edges.size());
+        // for(int i = 0 ; i <edges.size() ; i++){
+        //     adj[edges[i]].push_back(i);
+        // }
+        // map<int,long long>mpp;
+        // for(int i = 0 ; i < edges.size();i++){
+        //     for(auto it : adj[i]){
+        //         mpp[i] += it;
+        //     }
+        // }
+        // long long maxi  = -1 ;
+        // for(auto it : mpp){
+        //     maxi = max(maxi , it.second);
+        // }
+        // for(auto it : mpp){
+        //     if(it.second == maxi)return it.first;
+        // }
+        // return -1;
+        int n = edges.size();
+        vector<long long >score(n,0);
+        for(int i = 0 ; i<n ; i++){
+            score[edges[i]] += i;
         }
-        map<int,long long>mpp;
-        for(int i = 0 ; i < edges.size();i++){
-            for(auto it : adj[i]){
-                mpp[i] += it;
+        long long maxi = -1;
+        int ans = 0;
+        for(int i = 0; i <n ; i++){
+            if(score[i] > maxi){
+                maxi = score[i];
+                ans = i;
             }
         }
-        long long maxi  = -1 ;
-        for(auto it : mpp){
-            maxi = max(maxi , it.second);
-        }
-        for(auto it : mpp){
-            if(it.second == maxi)return it.first;
-        }
-        return -1;
+        return ans;
     }
 };
