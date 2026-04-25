36abe7a2b0b067eb1bc7ba429c3664209cfce7b0
Lucky Garg <lucky.work.005@gmail.com>
2026-04-25 22:11:12 +0530

    Time: 3 ms (73.88%) | Memory: 13.7 MB (74.28%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void solve(vector<vector<int>>&ans , vector<vector<int>>&adj , int s , vector<int>&temp){
+        temp.push_back(s);
+        if(s == adj.size()-1){
+            ans.push_back(temp);
+            temp.pop_back();
+            return ;
+        }
+        for(auto it : adj[s]){
+            solve(ans , adj , it , temp);
+        }
+        temp.pop_back();
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& adj) {
+        int n =adj.size();
+        vector<vector<int>>ans;
+        vector<int>temp;
+        solve(ans , adj , 0 , temp);
+        return ans;
+    }
+};
