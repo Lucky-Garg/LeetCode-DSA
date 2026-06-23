df082229acc90da04803d8f393654eb7a4a450f4
Lucky Garg <lucky.work.005@gmail.com>
2026-06-23 14:49:08 +0530

    Time: 24 ms (27.97%) | Memory: 29.7 MB (32.49%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char>st;
+        for(char c : s){
+            if(c == '*'){
+                if(!st.empty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(c);
+            }
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        int i = 0 , j = ans.size()-1;
+        while(i <= j){
+            swap(ans[i] , ans[j]);
+            i++ , j--;
+        }
+        return ans;
+    }
+};
