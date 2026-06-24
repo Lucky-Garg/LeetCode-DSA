4ac432fa1e8a193f80286cb3db1636df9683be86
Lucky Garg <cse23130@glbitm.ac.in>
2026-06-24 11:30:07 +0530

    Time: 47 ms (60.00%) | Memory: 54.8 MB (11.61%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool solve(int n,int m,string& s,string& p, vector<vector<int>> &dp){
+        if(n==0 && m==0) return true;
+        if(m==0) return false;
+        if(n==0){
+            for(int i=m-1;i>=0;i--){
+                if(p[i]!='*') return false;
+            }
+            return true;
+        }
+        if(dp[n][m]!=-1) return dp[n][m];
+        if(s[n-1]==p[m-1] || p[m-1]=='?') return dp[n][m]=solve(n-1,m-1,s,p,dp);
+        if(p[m-1]=='*') return dp[n][m]=solve(n-1,m,s,p,dp) || solve(n,m-1,s,p,dp);
+        return dp[n][m]=false;
+    }
+    bool isMatch(string s, string p) {
+        int n=s.length();
+        int m=p.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return solve(n,m,s,p,dp);
+    }
+};
