4ca3727d06d7e1ddf064ff8983165def3bdd654d
Lucky Garg <cse23130@glbitm.ac.in>
2026-07-06 17:11:03 +0530

    Time: 633 ms (9.52%) | Memory: 379.7 MB (6.14%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -30,13 +30,38 @@ public:
 
 class Solution {
 public:
+    bool dfs(int s , int d , unordered_map<int,vector<int>>&adj , vector<int>&vis){
+        if(s == d)return true;
+        vis[s] = 1;
+        for(auto v : adj[s]){
+            if(!vis[v]){
+                if(dfs(v , d , adj , vis))return true;
+            }
+        }
+        return false;
+    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        DSU ds(n);
+        // DSU ds(n);
+        // for(auto it : edges){
+        //     int u = it[0];
+        //     int v = it[1];
+        //     ds.unionBySize(u,v);
+        // }
+        // return (ds.findP(source) == ds.findP(destination));
+        // if(n==1)return true;
+        unordered_map<int,vector<int>>mpp;
         for(auto it : edges){
             int u = it[0];
             int v = it[1];
-            ds.unionBySize(u,v);
+            mpp[u].push_back(v);
+            mpp[v].push_back(u);
         }
-        return (ds.findP(source) == ds.findP(destination));
+        vector<int>vis(n , 0);
+        for(int i = 0 ; i < n ; i++){
+            if(!vis[i]){
+                if(dfs(source ,destination , mpp , vis))return true;
+            }
+        }
+        return false;
     }
 };
