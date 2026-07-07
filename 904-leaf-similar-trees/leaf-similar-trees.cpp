b0119e135a73e36b0e92c78150dfe5a5b9732d3b
Lucky Garg <cse23130@glbitm.ac.in>
2026-07-07 16:10:52 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (5.28%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.cpp
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -11,19 +11,20 @@
  */
 class Solution {
 public:
-    void leaf(TreeNode*r , vector<int>&store){
+    void leaf(TreeNode*r , string&s){
         if(r == NULL)return ;
         if(r->left == NULL && r->right == NULL){
-            store.push_back(r->val);
+            s += to_string(r->val)+"_";
             return ;
         }
-        leaf(r->left , store);
-        leaf(r->right , store);
+        leaf(r->left , s);
+        leaf(r->right , s);
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int>ans1 , ans2;
+        string ans1 , ans2;
         leaf(root1 , ans1);
         leaf(root2 , ans2);
+        cout << ans1 << " " << ans2;
         return ans1 == ans2;
     }
 };
