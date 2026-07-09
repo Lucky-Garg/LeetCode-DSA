3955e08de0e8cdb17ed48aa5b4461249e27a946a
Lucky Garg <cse23130@glbitm.ac.in>
2026-07-10 00:58:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (92.78%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    bool biPartite(int s ,vector<vector<int>>& adj , vector<int>&color , int currColor){
-        color[s] = currColor;
+    bool biPartite(int s ,vector<vector<int>>& adj , vector<int>&color ){
         for(auto v : adj[s]){
-            if(color[v] == color[s]){
-                return false;
+            if(color[v] == -1){
+                color[v] = 1 - color[s];
+                if(biPartite(v , adj , color))return true;
             }
-            else if(color[v] == -1){
-                int colorV = 1 - currColor;
-                if(biPartite(v , adj , color , colorV) == false)return false;
+            else {
+                if(color[v] == color[s])return true;
             }
         }
-        return true;
+        return false;
     }
     bool isBipartite(vector<vector<int>>& adj) {
         vector<int>color(adj.size() , -1);
-        for(int i = 0 ; i < adj.size() ; i++){
+        int n = adj.size();
+        for(int i = 0 ; i < n ; i++){
             if(color[i] == -1){
-                if(biPartite(i , adj , color , 1 ) == false)return false;
+                color[i] = 0;
+                if(biPartite(i , adj , color))return false;
             }
         }
         return true;
