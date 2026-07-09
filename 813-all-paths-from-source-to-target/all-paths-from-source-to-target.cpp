8c31e2b703361e71a6c6706ffa0dd22c0f8119ba
Lucky Garg <cse23130@glbitm.ac.in>
2026-07-10 01:17:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (61.90%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>&ans , vector<vector<int>>&adj , int s , vector<int>&temp){
-        temp.push_back(s);
-        if(s == adj.size()-1){
+    void solve(int u ,vector<vector<int>>& graph , vector<int>&temp ,vector<int>&vis , vector<vector<int>>& ans){
+        vis[u] = 1;
+        temp.push_back(u);
+        if(u == graph.size()-1){
             ans.push_back(temp);
+            vis[u] = 0;
         }
-        for(auto it : adj[s]){
-            solve(ans , adj , it , temp);
+        for(auto v : graph[u]){
+            if(!vis[v]){
+                solve(v , graph , temp , vis , ans);
+            }
         }
+        vis[u] = 0;
         temp.pop_back();
     }
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& adj) {
-        int n =adj.size();
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
         vector<vector<int>>ans;
         vector<int>temp;
-        solve(ans , adj , 0 , temp);
+        vector<int>vis(n,0);
+        solve(0, graph , temp , vis , ans );
         return ans;
     }
 };
