85d8e1b2ea69d7542d3d9fd8132c43f5d3720592
Lucky Garg <cse23130@glbitm.ac.in>
2026-07-15 18:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumOdd = 0;
-        int sumEven = 0;
-        for(int i = 1 ; i <=2*n ; i++){
-            if(i%2 == 1){
-                sumOdd += i;
-            }
-            else{
-                sumEven += i;
-            }
-        }
-        cout << sumOdd << " " << sumEven ;
-        return gcd(sumOdd , sumEven);
+        return n;
     }
 };
