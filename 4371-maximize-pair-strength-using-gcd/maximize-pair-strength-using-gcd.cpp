0a466aa563952910679156762b72a1c845ce5fa6
Lucky Garg <cse23130@glbitm.ac.in>
2026-08-02 23:33:52 +0530

    Time: 708 ms (94.68%) | Memory: 36.9 MB (86.19%) - LeetSync

--- 4371-maximize-pair-strength-using-gcd/maximize-pair-strength-using-gcd.cpp
+++ 4371-maximize-pair-strength-using-gcd/maximize-pair-strength-using-gcd.cpp
@@ -3,7 +3,7 @@ public:
     long long maxPairStrength(vector<int>& nums) {
         long long ans = -1;
         for(int i = 0 ; i< nums.size() ; i++){
-            for(int j = 0 ; j < nums.size() ; j++){
+            for(int j = i+1 ; j < nums.size() ; j++){
                 if(i!=j){
                     long long strength = ((long long)nums[i] * (long long)nums[j]) / pow(gcd(nums[i] , nums[j]) , 2);
                     if(ans < strength){
