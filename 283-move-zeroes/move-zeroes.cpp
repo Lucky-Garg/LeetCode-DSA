0e20690c0112cba0cc4ca9e51e8bdf941b89caed
Lucky Garg <cse23130@glbitm.ac.in>
2026-08-23 00:17:34 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (56.31%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int i = 0 , j = 1;
         int n = nums.size();
-        int i = 0 ;
-        for(int j = 0 ; j < nums.size() ; j++){
-            if(nums[j] != 0){
-                nums[i++] = nums[j];
+        while( j < n){
+            if(nums[i] == 0 && nums[j] == 0)j++;
+            else if(nums[i] == 0 && nums[j] != 0) {
+                swap(nums[i] , nums[j]);
+                i++ , j++;
             }
-        }
-        while(i<n){
-            nums[i++] = 0;
+            else{
+                i++ , j++;
+            }
         }
     }
 };
