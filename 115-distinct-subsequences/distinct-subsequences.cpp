f4594e523bf9f75695dc4618423a03e855507d2c
Lucky Garg <cse23130@glbitm.ac.in>
2026-09-06 16:23:00 +0530

    Time: 2810 ms (5.03%) | Memory: 421.4 MB (6.89%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
-    int fun(string s, string t , int i , int j , vector<vector<int>>&dp){
-        if(j < 0)return 1;
-        if(i < 0)return 0;
+    int solve(string s , string t , int i , int j , vector<vector<int>>&dp){
+        if(j >= t.size())return 1;
+        if(i >= s.size())return 0;
         if(dp[i][j] != -1)return dp[i][j];
         if(s[i] == t[j]){
-            return dp[i][j]= fun(s , t , i-1 , j-1 , dp) + fun(s , t , i-1 , j , dp);
+            return dp[i][j]  = solve(s , t, i+1 , j+1 , dp) + solve(s , t, i+1 , j , dp);
         }
-        else{
-            return dp[i][j] = fun(s , t , i-1 , j , dp);
-        }
+        else return dp[i][j] = solve(s , t, i+1 , j , dp);
     }
     int numDistinct(string s, string t) {
         int n = s.size() , m = t.size();
-        vector<vector<double>>dp(n+1, vector<double>(m+1));
-        for(int j = 0 ; j <= m ; j++){
-            dp[0][j] = 0;
-        }
-        for(int i = 0 ; i<= n ; i++){
-            dp[i][0] = 1;
-        }
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= m ; j++){
-                if(s[i-1] == t[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return (int)dp[n][m];
-        
+        vector<vector<int>>dp(n+1, vector<int>(m+1,-1));
+        return solve(s , t , 0 , 0 , dp);
     }
 };
