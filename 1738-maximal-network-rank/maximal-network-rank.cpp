c40b71acf43e42a07edbce490c2cb05314f2d51a
Lucky Garg <cse23130@glbitm.ac.in>
2026-09-17 00:01:17 +0530

    Time: 75 ms (7.59%) | Memory: 46.7 MB (13.21%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        unordered_map<int , vector<int>>adj;
+        unordered_map<int , unordered_set<int>>adj;
         for(auto it : roads){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+            adj[it[0]].insert(it[1]);
+            adj[it[1]].insert(it[0]);
         }
         int maxi = -1;
         for(int i = 0 ; i < n ; i++){
             int lenI = adj[i].size();
             for(int j = i+1 ; j < n ; j++){
                 int lenJ = adj[j].size();
-                if(find(adj[i].begin() , adj[i].end() , j) != adj[i].end()){
+                if(adj[i].count(j)){
                     lenJ -= 1;
                 }
                 maxi = max(maxi , lenI + lenJ);
